5dbe0e191f157defc587608845d6d87704f916cb
Daniel Musketa <daniel@musketa.de>
2021-05-11 11:52:17 +0200

    urls

--- form_with_split_date_time/form_with_split_date_time/urls.py
+++ form_with_split_date_time/form_with_split_date_time/urls.py
@@ -16,6 +16,21 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+from demoapp import views
+
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+
+    path(
+        "",
+        views.IndexView.as_view(),
+        name="index",
+    ),
+
+    path(
+        "create/",
+        views.UniverseCreate.as_view(),
+        name="create",
+    ),
 ]
