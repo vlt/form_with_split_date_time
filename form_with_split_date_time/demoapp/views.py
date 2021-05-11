6a12bb59e1e2d14aef9be4491c4c312865131b64
Daniel Musketa <daniel@musketa.de>
2021-05-11 11:40:46 +0200

    views

--- form_with_split_date_time/demoapp/views.py
+++ form_with_split_date_time/demoapp/views.py
@@ -1,3 +1,21 @@
-from django.shortcuts import render
+from django.urls import reverse_lazy
+from django.views.generic import ListView
+from django.views.generic.edit import CreateView
 
-# Create your views here.
+from .forms import (
+    UniverseForm,
+)
+
+from .models import (
+    Universe,
+)
+
+
+class IndexView(ListView):
+    model = Universe
+
+
+class UniverseCreate(CreateView):
+    model = Universe
+    form_class = UniverseForm
+    success_url = reverse_lazy("index")
